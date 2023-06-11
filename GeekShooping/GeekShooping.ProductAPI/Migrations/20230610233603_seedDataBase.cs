ee9361df216f6e9b4fa0378ba0221857723d79fb
Budula <williammalheiros_2@hotmail.com>
2023-06-11 13:00:04 +0200

    SeedDataBase - using the command "Add-Migration seedDataBase" and  "UpdateDatabase"

---
+++ GeekShooping/GeekShooping.ProductAPI/Migrations/20230610233603_seedDataBase.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GeekShooping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 4L, "T-shirt", "Camiseta Simpsons", "https://www.stargeek.es/1266-large_default/comprar-camiseta-homer-simpson-biceps-personalizada-ropa-online-de-videojuegos.jpg", "Camiseta Homer Simpsons", 159.9m },
+                    { 5L, "T-shirt", "Camiseta Bart Simpsons", "https://sublistamp.com/11171-large_default/camiseta-de-bart-simpson.jpg", "Camiseta Bart Simpsons", 250.9m },
+                    { 6L, "COFFE MUG", "Caneca Bart Simpsons", "https://i.zst.com.br/thumbs/51/e/32/1484288505.jpg", "Caneca Bart", 89.9m },
+                    { 7L, "COFFE MUG", "Caneca Hommer simpsons", "https://m.media-amazon.com/images/I/71tZUZ6SmyL._AC_UF894,1000_QL80_.jpg", "Caneca Hommer", 39.9m },
+                    { 8L, "COFFE MUG", "Caneca Programmer", "https://i.pinimg.com/originals/0e/a6/63/0ea66335d03125fe176c9dfa1101e719.png", "Caneca Programmer", 159.9m },
+                    { 9L, "COFFE MUG", "Caneca StarWars", "https://m.media-amazon.com/images/I/41fnon3MqbL._AC_UF894,1000_QL80_.jpg", "Caneca StarWars", 69.9m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 8L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 9L);
+        }
+    }
+}
--- GeekShooping/GeekShooping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShooping/GeekShooping.ProductAPI/Model/Context/MySQLContext.cs
@@ -9,6 +9,65 @@ namespace GeekShooping.ProductAPI.Model.Context
         }
         public MySQLContext(DbContextOptions<MySQLContext> options):base(options) { }
        
-        public DbSet<Product> Products { get; set; } 
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Camiseta Homer Simpsons",
+                Price = 159.9M,
+                Description = "Camiseta Simpsons",
+                ImageUrl = "https://www.stargeek.es/1266-large_default/comprar-camiseta-homer-simpson-biceps-personalizada-ropa-online-de-videojuegos.jpg",
+                CategoryName = "T-shirt",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Camiseta Bart Simpsons",
+                Price = 250.9M,
+                Description = "Camiseta Bart Simpsons",
+                ImageUrl = "https://sublistamp.com/11171-large_default/camiseta-de-bart-simpson.jpg",
+                CategoryName = "T-shirt",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Caneca Bart",
+                Price = 89.9M,
+                Description = "Caneca Bart Simpsons",
+                ImageUrl = "https://i.zst.com.br/thumbs/51/e/32/1484288505.jpg",
+                CategoryName = "COFFE MUG",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 7,
+                Name = "Caneca Hommer",
+                Price = 39.9M,
+                Description = "Caneca Hommer simpsons",
+                ImageUrl = "https://m.media-amazon.com/images/I/71tZUZ6SmyL._AC_UF894,1000_QL80_.jpg",
+                CategoryName = "COFFE MUG",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 8,
+                Name = "Caneca Programmer",
+                Price = 159.9M,
+                Description = "Caneca Programmer",
+                ImageUrl = "https://i.pinimg.com/originals/0e/a6/63/0ea66335d03125fe176c9dfa1101e719.png",
+                CategoryName = "COFFE MUG",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 9,
+                Name = "Caneca StarWars",
+                Price = 69.9M,
+                Description = "Caneca StarWars",
+                ImageUrl = "https://m.media-amazon.com/images/I/41fnon3MqbL._AC_UF894,1000_QL80_.jpg",
+                CategoryName = "COFFE MUG",
+            });
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
