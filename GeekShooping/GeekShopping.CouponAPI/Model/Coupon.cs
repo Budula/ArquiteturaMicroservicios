0e0105a7d2806b38876b8b4bc0211482c29154a3
Budula <williammalheiros_2@hotmail.com>
2023-10-08 16:42:37 +0200

    CouponAPI

---
+++ GeekShooping/GeekShopping.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,20 @@
+﻿using GeekShooping.CouponAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CouponAPI.Model
+{
+    [Table("coupon")]
+    public class Coupon : BaseEntity
+    {   
+        [Column("counpon_code")]
+        [Required]
+        [StringLength(30)]
+        public string CounponCode { get; set; }
+
+        [Column("discount_amount")]
+        [Required]       
+        public decimal DiscountAmount { get; set; }
+
+    }
+}
---
+++ GeekShooping/GeekShopping.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using GeekShooping.CouponPI.Model.Context;
+using GeekShopping.CouponAPI.Data.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly MySQLContext _context;
+        private IMapper _mapper;
+
+        public CouponRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<CouponVO> GetCouponByCouponCode(string couponCode)
+        {
+            var coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.CounponCode == couponCode);
+
+            return _mapper.Map<CouponVO>(coupon);
+        }
+    }
+}
